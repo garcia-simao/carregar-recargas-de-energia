bbe63e05716a97b53f8ba8555702cd92ed439630
garcia-simao <silviosimons@gmail.com>
2024-12-20 15:04:15 +0100

    alteracoes nos models

--- carregar_energia_api/carregar_energia_api/settings.py
+++ carregar_energia_api/carregar_energia_api/settings.py
@@ -30,7 +30,7 @@ FIREBASE_CREDENTIALS2 = '/home/garcia_simao/Documentos/Projectos_de_trabalho/cha
 
 
 #inicializar o firebase admin sdk
-cred = credentials.Certificate(FIREBASE_CREDENTIALS)
+cred = credentials.Certificate(FIREBASE_CREDENTIALS2)
 firebase_admin.initialize_app(cred)
 
 
--- carregar_energia_api/core/models.py
+++ carregar_energia_api/core/models.py
@@ -19,7 +19,11 @@ class Usuario(models.Model):
     numero_do_contador = models.IntegerField()
     data_criacao = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.nome
     
+
+
 class CarregarRecarga(models.Model):
     id_do_usuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
     codigo_da_recarga = models.IntegerField()
