7bab3082d9e27a2b0979e8c66f3e050d73448035
garcia-simao <silviosimons@gmail.com>
2025-03-05 14:30:47 +0100

    Atualização da configuração do firebase e troca do banco de dados.

--- carregar_energia_api/carregar_energia_api/settings.py
+++ carregar_energia_api/carregar_energia_api/settings.py
@@ -104,15 +104,16 @@ WSGI_APPLICATION = 'carregar_energia_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-"""""""""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""""
 
+
+"""""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -127,7 +128,7 @@ DATABASES = {
         }
     }
 }
-
+"""""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- carregar_energia_api/core/firebase_config.py
+++ carregar_energia_api/core/firebase_config.py
@@ -4,6 +4,7 @@ firebaseConfig = {
     "apiKey": "AIzaSyDSFM4hU69Sm9ny4BWmNPBoqXWxexXPQv0",
     "authDomain": "carregar-regarcas-ende.firebaseapp.com",
     "projectId": "carregar-regarcas-ende",
+     "databaseURL": "https://carregar-regarcas-ende-default-rtdb.firebaseio.com/",
     "storageBucket": "carregar-regarcas-ende.firebasestorage.app",
     "messagingSenderId": "517171228824",
     "appId": "1:517171228824:web:2b64a6028d2b1ba4c320ed",
